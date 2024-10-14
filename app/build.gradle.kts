a9050be24283e41a56527fe57f94932a30d1a8c0
PolRubiano <diego@pigames.es>
2024-10-14 19:56:37 +0200

    241014_añadidas las librerias glide y gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.glide)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ app/src/main/java/edu/iesam/superheroes/presentation/SuperHeroesActivity.kt
@@ -0,0 +1,12 @@
+package edu.iesam.superheroes.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.iesam.superheroes.R
+
+class SuperHeroesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
