368fbc5f8520fdfe890c3b98c84da78465043090
PolRubiano <diego@pigames.es>
2024-10-16 18:30:53 +0200

    241016_added superheroes activity, added internet permissions an android manifest, update url images, compiled and working

--- app/src/main/java/edu/iesam/superheroes/data/remote/SuperHeroMockRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/superheroes/data/remote/SuperHeroMockRemoteDataSource.kt
@@ -5,10 +5,10 @@ import edu.iesam.superheroes.domain.SuperHeroe
 class SuperHeroMockRemoteDataSource {
 
     private val superHeroes = listOf(
-        SuperHeroe("1", "superheroe1", "https://www.google.es"),
-        SuperHeroe("2", "superheroe2", "https://www.google.es"),
-        SuperHeroe("3", "superheroe3", "https://www.google.es"),
-        SuperHeroe("4", "superheroe4", "https://www.google.es"),
+        SuperHeroe("1", "superheroe1", "https://pics.filmaffinity.com/the_sting-433653100-mmed.jpg"),
+        SuperHeroe("2", "superheroe2", "https://pics.filmaffinity.com/the_sting-433653100-mmed.jpg"),
+        SuperHeroe("3", "superheroe3", "https://pics.filmaffinity.com/the_sting-433653100-mmed.jpg"),
+        SuperHeroe("4", "superheroe4", "https://pics.filmaffinity.com/the_sting-433653100-mmed.jpg")
     )
 
     fun getSuperHeroes(): List<SuperHeroe> {
--- app/src/main/java/edu/iesam/superheroes/presentation/SuperHeroesActivity.kt
+++ app/src/main/java/edu/iesam/superheroes/presentation/SuperHeroesActivity.kt
@@ -1,12 +1,55 @@
 package edu.iesam.superheroes.presentation
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import edu.iesam.superheroes.R
+import edu.iesam.superheroes.domain.SuperHeroe
 
 class SuperHeroesActivity : AppCompatActivity() {
+
+    private lateinit var superHeroFactory: SuperHeroFactory
+    private lateinit var viewModel: SuperHeroesViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_superheroes)
+
+        superHeroFactory = SuperHeroFactory(this)
+        viewModel = superHeroFactory.buildViewModel()
+
+        val superheroes = viewModel.viewCreated()
+        bindData(superheroes)
+    }
+
+    private fun bindData(superHeroes: List<SuperHeroe>) {
+        findViewById<TextView>(R.id.superHero_id_1).text = superHeroes[0].id
+        findViewById<TextView>(R.id.superHero_name_1).text = superHeroes[0].name
+        findViewById<LinearLayout>(R.id.layout_1).setOnClickListener {
+            navigateToSuperHeroDetail(superHeroes[0].id)
+        }
+
+        findViewById<TextView>(R.id.superHero_id_2).text = superHeroes[1].id
+        findViewById<TextView>(R.id.superHero_name_2).text = superHeroes[1].name
+        findViewById<LinearLayout>(R.id.layout_2).setOnClickListener {
+            navigateToSuperHeroDetail(superHeroes[1].id)
+        }
+
+        findViewById<TextView>(R.id.superHero_id_3).text = superHeroes[2].id
+        findViewById<TextView>(R.id.superHero_name_3).text = superHeroes[2].name
+        findViewById<LinearLayout>(R.id.layout_3).setOnClickListener {
+            navigateToSuperHeroDetail(superHeroes[2].id)
+        }
+
+        findViewById<TextView>(R.id.superHero_id_4).text = superHeroes[2].id
+        findViewById<TextView>(R.id.superHero_name_4).text = superHeroes[2].name
+        findViewById<LinearLayout>(R.id.layout_4).setOnClickListener {
+            navigateToSuperHeroDetail(superHeroes[2].id)
+        }
+    }
+
+    private fun navigateToSuperHeroDetail(superHeroId: String) {
+        startActivity(SuperHeroDetailActivity.getIntent(this, superHeroId))
     }
 }
